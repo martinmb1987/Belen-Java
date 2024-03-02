e2848e94e56d318e49114c1473644e904eecf65d
martinmb1987 <martinmb1987@gmail.com>
2024-03-02 16:00:56 -0500

    Java practice

---
+++ src/belen.java
@@ -0,0 +1,12 @@
+public class belen {
+    public static void main(String[] args) {
+        // this is for practice
+        // the short cut for printing a line is sout and then click tab
+        System.out.println("This is a great practice");
+        System.out.println("felipe is the best dog");
+        System.out.println("my boyfriend goes to the bathroom too much");
+        /*
+        For creating a multi line comment /* click enter
+         */
+    }
+    }
